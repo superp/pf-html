b20eb6a87022d1a0732bb5291cdae4a0e86f8a7f
evjeniy <metall.92@inbox.ru>
2020-09-23 16:35:36 +0300

    add mobile

---
+++ dist/assets/js/main.js
@@ -0,0 +1,21 @@
+(function() {
+  var initMenu;
+
+  initMenu = function() {
+    $('.menu_btn').on('click', function() {
+      return $('body').toggleClass('menu-opened');
+    });
+    return $(document).mouseup(function(e) {
+      var menu;
+      menu = $('.navigation, .menu_btn');
+      if (!menu.is(e.target) && menu.has(e.target).length === 0) {
+        $('body').removeClass('menu-opened');
+      }
+    });
+  };
+
+  $(function() {
+    return initMenu();
+  });
+
+}).call(this);
---
+++ src/coffee/main.coffee
@@ -0,0 +1,12 @@
+initMenu = ->  
+  $('.menu_btn').on 'click', ->
+    $('body').toggleClass('menu-opened')
+
+  $(document).mouseup (e) ->
+    menu = $('.navigation, .menu_btn')
+    if !menu.is(e.target) and menu.has(e.target).length == 0
+      $('body').removeClass 'menu-opened'
+    return
+
+$ ->
+  initMenu()
