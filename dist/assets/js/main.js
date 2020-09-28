8670a578d34c3f20c21a3f746675480a02b3eff2
evjeniy <metall.92@inbox.ru>
2020-09-28 12:19:42 +0300

    upd

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,19 +1,21 @@
-(function() {
-  var initMenu;
+var initMenu;
 
-  initMenu = function() {
-    $('.menu_btn').on('click', function() {
-      return $('body').toggleClass('menu-opened');
-    });
-    return $(document).mouseup(function(e) {
-      var menu;
-      menu = $('.navigation, .menu_btn');
-      if (!menu.is(e.target) && menu.has(e.target).length === 0) {
-        $('body').removeClass('menu-opened');
-      }
-    });
-  };
+initMenu = function() {
+  $('.menu_btn').on('click', function() {
+    return $('body').toggleClass('menu-opened');
+  });
+  return $(document).mouseup(function(e) {
+    var menu;
+    menu = $('.navigation, .menu_btn');
+    if (!menu.is(e.target) && menu.has(e.target).length === 0) {
+      $('body').removeClass('menu-opened');
+    }
+  });
+};
 
+$(function() {
+  return initMenu();
+});
   $(function() {
     return initMenu();
   });
